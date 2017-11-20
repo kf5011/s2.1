6379dd404b7058f49bb3a33500bd34e747ec21b1
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-11-20 11:01:12 +0000

    tidied up code for release

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,25 @@
 #include <mbed.h>
 
-
-class LED {
-    DigitalOut pin;
-public:
-    LED(PinName p) : pin(p) {off();}
-    void on (void) { pin.write(0);}
-    void off(void) { pin.write(1);}
-};
-
-LED   red(LED_RED);
-LED green(LED_GREEN);
+DigitalOut   red(LED_RED);
+DigitalOut green(LED_GREEN);
 
 void on(void)
 {
-    red.on();
+    red.write(0);
     puts("rising 0->1");
 }
 void off(void)
 {
-    red.off();
+    red.write(1);
     puts("falling 1->0");
 }
 
 int main() {
     InterruptIn  left(SW2);
     InterruptIn right(SW3);
-    // put your setup code here, to run once:
 
     left.rise(on);
     right.fall(off);
     while(1) {
-        // put your main code here, to run repeatedly:
-
     }
 }
