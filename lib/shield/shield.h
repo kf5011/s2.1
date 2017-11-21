6ba0b2f2a7cd58ab2f03b7ebd8b702905a88c991
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-11-21 03:28:58 +0000

    shield pins

---
+++ lib/shield/shield.h
@@ -0,0 +1,24 @@
+enum {
+    POT_1 = A0,
+    POT_2 = A1,
+    SW_UP = A2,
+    SW_DOWN = A3,
+    SW_LEFT = A4,
+    SW_RIGHT = A5,
+
+    LCD_SCK = D13,
+    LCD_RESET = D12,
+    LCD_MOSI = D11,
+    LCD_CS_N = D10,
+    SHIELD_LED_GREEN = D9,
+    SHIELD_LED_BLUE = D8,
+
+    LCD_A0 = D7,
+    SPEAKER = D6,
+    SHIELD_LED_RED = D5,
+    SW_CENTER = D4,
+    XBEE_NRST = D3,
+    XBEE_STATUS = D3,
+    XBEE_RX = D1,
+    XBEE_TX = D0
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,26 @@
 #include <mbed.h>
 
-
-class LED {
-    DigitalOut pin;
-public:
-    LED(PinName p) : pin(p) {off();}
-    void on (void) { pin.write(0);}
-    void off(void) { pin.write(1);}
-};
-
-LED   red(LED_RED);
-LED green(LED_GREEN);
+DigitalOut   red(LED_RED,1);
+DigitalOut green(LED_GREEN,1);
 
 void on(void)
 {
-    red.on();
-    puts("rising 0->1");
+    red.write(1);
+    puts("left rising 0->1");
 }
 void off(void)
 {
-    red.off();
-    puts("falling 1->0");
+    red.write(0);
+    puts("right falling 1->0");
 }
 
 int main() {
-    InterruptIn  left(SW2);
-    InterruptIn right(SW3);
-    // put your setup code here, to run once:
+    InterruptIn  left(SW3);
+    InterruptIn right(SW2);
 
     left.rise(on);
     right.fall(off);
     while(1) {
-        // put your main code here, to run repeatedly:
-
+        /* GNDN */
     }
 }
